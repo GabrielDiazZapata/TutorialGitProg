d9c50700019045c8cdd67e84b514a94630cb500c
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-03-28 10:46:16 +0100

    gjk

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-    final Scanner SCANNER = new Scanner(System.in);
-    System.out.println("Introduce un número");
-    String number = SCANNER.nextLine();
-    System.out.println("Hola Mundo " + number + "!");
-    SCANNER.close();
+    System.out.println("Hola Mundo ");
+    
 
     }
 }   
